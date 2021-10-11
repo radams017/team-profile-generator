db5499c33a9bbdafdd8ef9d7185c0e3e08b7a69d
Ryan Adams <ryanadams14@gmail.com>
2021-10-11 00:37:00 -0500

    adding sample index.html and some css. everything is done now

--- src/createTeamPage.js
+++ src/createTeamPage.js
@@ -5,46 +5,52 @@ function createCards(team) {
     switch (emp.getRole()) {
       case "Engineer":
         cards += `
-        <div class="card col" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${emp.getName()}</h5>
-                <h5>${emp.getRole()}</h5>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Employee ID: ${emp.getID()}</li>
-                <li class="list-group-item"><a href="https://www.github.com/${emp.getGit()}" class="card-link">GitHub: ${emp.getGit()}</a></li>
-                <li class="list-group-item"><a href="mailto:${emp.getEmail()}" class="card-link">Email: ${emp.getEmail()}</a></li>
-            </ul>
+        <div class="col-6 mb-4">
+          <div class="card c-shadow">
+              <div class="card-body">
+                  <h5 class="card-title">${emp.getName()}</h5>
+                  <h5>${emp.getRole()}</h5>
+              </div>
+              <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Employee ID: ${emp.getID()}</li>
+                  <li class="list-group-item">GitHub: <a href="https://www.github.com/${emp.getGit()}" class="card-link">${emp.getGit()}</a></li>
+                  <li class="list-group-item">Email: <a href="mailto:${emp.getEmail()}" class="card-link">${emp.getEmail()}</a></li>
+              </ul>
+          </div>
         </div>
     `;
         break;
       case "Intern":
         cards += `
-          <div class="card col" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${emp.getName()}</h5>
-                <h5>${emp.getRole()}</h5>
-            </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">Employee ID: ${emp.getID()}</li>
-            <li class="list-group-item">School: ${emp.getSchool()}</li>
-            <li class="list-group-item"><a href="mailto:${emp.getEmail()}" class="card-link">Email: ${emp.getEmail()}</a></li>
-          </ul>
+        <div class="col-6 mb-4">
+          <div class="card c-shadow">
+              <div class="card-body">
+                  <h5 class="card-title">${emp.getName()}</h5>
+                  <h5>${emp.getRole()}</h5>
+              </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Employee ID: ${emp.getID()}</li>
+              <li class="list-group-item">School: ${emp.getSchool()}</li>
+              <li class="list-group-item">Email: <a href="mailto:${emp.getEmail()}" class="card-link">${emp.getEmail()}</a></li>
+            </ul>
+          </div>
         </div>
       `;
         break;
       case "Manager":
         cards += `
-            <div class="card col" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">${emp.getName()}</h5>
-                     <h5>${emp.getRole()}</h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Employee ID: ${emp.getID()}</li>
-                    <li class="list-group-item">Office #: ${emp.getOffice()}</li>
-                    <li class="list-group-item"><a href="mailto:${emp.getEmail()}" class="card-link">Email: ${emp.getEmail()}</a></li>
-                </ul>
+          <div class="col-6 mb-4">
+            <div class="card c-shadow">
+                  <div class="card-body">
+                      <h5 class="card-title">${emp.getName()}</h5>
+                      <h5>${emp.getRole()}</h5>
+                  </div>
+                  <ul class="list-group list-group-flush">
+                      <li class="list-group-item">Employee ID: ${emp.getID()}</li>
+                      <li class="list-group-item">Office #: ${emp.getOffice()}</li>
+                      <li class="list-group-item">Email: <a href="mailto:${emp.getEmail()}" class="card-link">${emp.getEmail()}</a></li>
+                  </ul>
+            </div>
           </div>
         `;
         break;
@@ -63,11 +69,12 @@ function createPage(cards) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
 <body>
     <div class="container text-center">
-        <div class="bg-primary">
+        <div class="team-header m-5">
             <h2>Team Manager</h2>
         </div>
         <div class="container">
